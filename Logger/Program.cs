dccff4426c5c36760c74d0851b067799953f3311
simonhodne <nomis.hodne@gmail.com>
2024-04-29 19:47:05 +0200

    Add project files.

---
+++ Logger/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.IO.Ports;
+
+namespace Logger
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            using (SerialPort port = new SerialPort("COM3", 921600))
+            {
+                
+                port.Open();
+
+                while (true)
+                {
+                    if(port.BytesToRead > 0)
+                    {
+                        using (StreamWriter writer = new(DateTime.Now.ToString() + ".txt", append: true))
+                        {
+                            while (true)
+                            {
+                                if (port.BytesToRead > 0)
+                                {
+                                    string logEntry = port.ReadLine();
+                                    writer.WriteLine(logEntry);
+                                    writer.Flush();
+                                    if(logEntry == "[END]")
+                                    {
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    
+                }
+            }
+        }
+    }
+}
